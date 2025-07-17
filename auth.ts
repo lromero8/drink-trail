3396c8eabf5427d714c1e8013c38a9027b564ce6
Luis Romero <luis.romero@atlascopco.com>
2025-07-17 15:38:47 +0200

    fix(auth): Adjust catch block formatting in getUser function

--- auth.ts
+++ auth.ts
@@ -12,7 +12,8 @@ async function getUser(email: string): Promise<User | undefined> {
     try {
         const user = await sql<User[]>`SELECT * FROM users WHERE email=${email}`;
         return user[0];
-    } catch (error) {
+    }
+    catch (error) {
         console.error('Failed to fetch user:', error);
         throw new Error('Failed to fetch user.');
     }
