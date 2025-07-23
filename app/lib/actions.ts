eba013a5e7b9377a73c342a40ac3a613ce6d6458
Luis Romero <luis.romero@atlascopco.com>
2025-07-23 10:09:43 +0200

    feat(db): Create trails table.

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -2,6 +2,13 @@
 
 import { signIn } from '../../auth';
 import { AuthError } from 'next-auth';
+import postgres from 'postgres';
+import bcrypt from 'bcrypt';
+import { redirect } from 'next/navigation';
+import { revalidatePath } from 'next/cache';
+
+const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
+
 
 export async function authenticate(
     prevState: string | undefined,
@@ -21,4 +28,30 @@ export async function authenticate(
         }
         throw error;
     }
+}
+
+export async function createTrail() {
+    const newTrail = {
+        id: generateRandomId(),
+        name: '',
+        locations: [],
+        createdAt: new Date().toISOString().split('T')[0]
+    };
+    
+    try {
+        await sql`
+            INSERT INTO trails (id, name, locations, createdAt)
+            VALUES (${newTrail.id}, ${newTrail.name}, ${newTrail.locations}, ${newTrail.createdAt})
+        `;
+    
+    }
+    catch (error) {
+        console.error(error);
+    }
+    revalidatePath(`/dashboard/trail/${newTrail.id}`);
+    redirect(`/dashboard/trail/${newTrail.id}`);
+}
+
+function generateRandomId() {
+    return bcrypt.hashSync(Math.random().toString(), 10);
 }
