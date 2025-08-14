8f83c2127553aa4b54c8b8f1fa0c3237e8e9f369
Luis Romero <luis.romero@atlascopco.com>
2025-08-14 14:33:03 +0200

    feat(login): Add DrinkTrailLogo component and logo image in login form.

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,5 +1,6 @@
 import LoginForm from '@/app/ui/login-form';
 import { Suspense } from 'react';
+import DrinkTrailLogo from '@/app/ui/drink-trail-logo';
 
 export default function LoginPage() {
     return (
@@ -7,7 +8,7 @@ export default function LoginPage() {
             <div className="relative mx-auto flex w-full max-w-[400px] flex-col space-y-2.5 p-4 md:-mt-32">
                 <div className="flex h-20 w-full items-end rounded-lg bg-blue-500 p-3 md:h-36">
                     <div className="w-32 text-white md:w-36">
-                        Logo
+                        <DrinkTrailLogo />
                     </div>
                 </div>
                 <Suspense>
---
+++ app/ui/drink-trail-logo.tsx
@@ -0,0 +1,17 @@
+
+import Image from 'next/image';
+
+export default function DrinkTrailLogo() {
+    return (
+        <div className="flex flex-row items-center leading-none text-white">
+            <Image
+                src="/logo.png"
+                width={60}
+                height={60}
+                className="w-[60px] h-[60px] md:w-[80px] md:h-[80px]"
+                alt="logo of Drink Trail"
+            />
+            <span className="text-[25px] md:text-[30px] whitespace-nowrap ml-3 mt-5">Drink Trail</span>
+        </div>
+    );
+}
