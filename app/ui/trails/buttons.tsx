f91643e1e7bf636e418a4d26c17f0a4b66f7ab4e
Luis Romero <luis.romero@atlascopco.com>
2025-08-15 10:40:27 +0200

    feat(client): Update button and pagination styles to use orange color scheme.

--- app/ui/trails/buttons.tsx
+++ app/ui/trails/buttons.tsx
@@ -6,7 +6,7 @@ export function CreateTrail() {
   return (
     <Link
       href="/dashboard/trails/create"
-      className="flex h-10 items-center rounded-lg bg-blue-600 px-4 text-sm font-medium text-white transition-colors hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600"
+      className="flex h-10 items-center rounded-lg bg-orange-500 px-4 text-sm font-medium text-white transition-colors hover:bg-orange-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-orange-500"
     >
       <span className="hidden md:block">Create Trail</span>{' '}
       <PlusIcon className="h-5 md:ml-4" />
