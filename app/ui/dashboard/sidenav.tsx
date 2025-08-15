52bfb5d4c3c5a446823b3b3f6d1ed7eb478bd3af
Luis Romero <luis.romero@atlascopco.com>
2025-08-15 10:36:05 +0200

    feat(client): Update side navigation and logo styles to use orange color scheme.

--- app/ui/dashboard/sidenav.tsx
+++ app/ui/dashboard/sidenav.tsx
@@ -2,14 +2,18 @@ import Link from 'next/link';
 import NavLinks from '@/app/ui/dashboard/nav-links';
 import { PowerIcon } from '@heroicons/react/24/outline';
 import { signOut } from '@/auth';
+import DrinkTrailLogo from '@/app/ui/drink-trail-logo';
 
 export default function SideNav() {
   return (
     <div className="flex h-full flex-col px-3 py-4 md:px-2">
       <Link
-        className="mb-2 flex h-20 items-end justify-start rounded-md bg-blue-600 p-4 md:h-40"
+        className="mb-2 flex h-20 items-end justify-start rounded-md bg-orange-500 p-4 md:h-40"
         href="/"
       >
+        <div className="flex text-center justify-center align-middle">
+          <DrinkTrailLogo />
+        </div>
       </Link>
       <div className="flex grow flex-row justify-between space-x-2 md:flex-col md:space-x-0 md:space-y-2">
         <NavLinks />
@@ -20,7 +24,7 @@ export default function SideNav() {
             await signOut({ redirectTo: '/' });
           }}
         >
-          <button className="flex h-[48px] w-full grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3">
+          <button className="flex h-[48px] w-full grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-orange-100 hover:text-orange-500 md:flex-none md:justify-start md:p-2 md:px-3">
             <PowerIcon className="w-6" />
             <div className="hidden md:block">Sign Out</div>
           </button>
--- app/ui/drink-trail-logo.tsx
+++ app/ui/drink-trail-logo.tsx
@@ -11,7 +11,7 @@ export default function DrinkTrailLogo() {
                 className="w-[60px] h-[60px] md:w-[80px] md:h-[80px]"
                 alt="logo of Drink Trail"
             />
-            <span className="text-[25px] md:text-[30px] whitespace-nowrap ml-3 mt-5">Drink Trail</span>
+            <span className="text-[25px] md:text-[28px] whitespace-nowrap ml-1 mt-5">Drink Trail</span>
         </div>
     );
 }
